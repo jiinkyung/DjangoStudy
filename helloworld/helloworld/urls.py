34945c883d7daf19d3589396bf04b57e049db49a
jiinkyung <jinkyung010310@gmail.com>
2022-05-29 13:14:49 +0900

    django 시작하기

---
+++ helloworld/helloworld/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+import myapp.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', myapp.views.home), # http://127.0.0.1:8000/ 라는 url로 들어가면 myapp.views.home 실행시켜라!
+    path('test/', myapp.views.test)
+]
