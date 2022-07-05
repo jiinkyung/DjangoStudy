cf5c1d8190335b9f5231aeef01ff527a75023122
jiinkyung <jinkyung010310@gmail.com>
2022-07-05 21:23:18 +0900

    회사 소개 사이트 만들기

--- [Project1]_회사소개사이트/cafeproject/settings.py
+++ [Project1]_회사소개사이트/cafeproject/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -104,9 +105,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICSFILES_DIRS = [
-    BASE_DIR / 'static'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
