c86980c04411d187fba38dd2efdca40f021c3316
jiinkyung <jinkyung010310@gmail.com>
2022-07-08 00:52:18 +0900

    login 구현

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogapp'
+    'blogapp',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -89,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+LOGIN_REDIRECT_URL = "/"
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from blogapp import views
+from accounts import views as account_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -21,6 +22,9 @@ urlpatterns = [
     path('detail/<int:blog_id>', views.detail, name='detail'),   
 
     path('create_comment/<int:blog_id>', views.create_comment, name='create_comment'),
+
+    path('login/', account_views.login, name='login'),
+    path('logout/', account_views.logout, name='logout'),
 ]
 
 # media 파일에 접근할 수 있는 url도 추가해주어야 함.
