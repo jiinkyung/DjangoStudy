10909130d2875cf2829d61c589d75f6be7032784
jiinkyung <jinkyung010310@gmail.com>
2022-07-14 15:15:08 +0900

    회원가입 구현

--- devsns/accounts/views.py
+++ devsns/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 def login(request):
     # request == POSt
@@ -23,3 +24,14 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect('home')
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST['password'] == request.POST['repeat']:
+            # 회원가입
+            new_user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            # 로그인
+            auth.login(request, new_user)
+            # 홈 리다이렉션
+            return redirect('home')
+    return render(request, 'register.html')
