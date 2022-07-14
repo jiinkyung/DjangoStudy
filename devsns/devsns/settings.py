9450a847a8214301c4e8bb3a4c38fe8ad82d8bae
jiinkyung <jinkyung010310@gmail.com>
2022-07-14 15:58:33 +0900

    외부 db 연동

--- devsns/devsns/settings.py
+++ devsns/devsns/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from .mysettings import mySECRET_KEY, myDATABASES
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '###################################################'
+SECRET_KEY = mySECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -63,12 +63,7 @@ WSGI_APPLICATION = 'devsns.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = myDATABASES
 
 
 # Password validation
