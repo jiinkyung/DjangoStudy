400116725c3b6aad02934e96548d9905f03e84c9
jiinkyung <jinkyung010310@gmail.com>
2022-07-14 12:59:15 +0900

    댓글 구현

--- devsns/devsns/urls.py
+++ devsns/devsns/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('postcreate/', views.postcreate, name='postcreate'),
-    path('detail/<int:post_id>', views.detail, name='detail')
+    path('detail/<int:post_id>', views.detail, name='detail'),
+    path('new_comment/<int:post_id>', views.new_comment, name='new_comment'),
+     
 ]
