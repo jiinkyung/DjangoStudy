7d0ccb5d1b88160c5ce52a36b7c0422f16718f5d
jiinkyung <jinkyung010310@gmail.com>
2022-07-14 17:55:39 +0900

    최신 영화 데이터 가져오기

---
+++ movieproj/movieapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import requests
+import json
+
+my_id='f2a8424e40bf3e57b995e2c750a52465'
+
+def home(request):
+    url = 'https://api.themoviedb.org/3/trending/movie/week?api_key='+my_id
+    response = requests.get(url)
+    resdata = response.text
+    obj = json.loads(resdata)
+    obj = obj['results']
+
+    return render(request, 'index.html', {'obj':obj})
+
+def detail(request, movie_id):
+    # https://api.themoviedb.org/3/movie/300?api_key=f2a8424e40bf3e57b995e2c750a52465&language=en-US
+    url = 'https://api.themoviedb.org/3/movie/' + movie_id +'?api_key=' + my_id
+  
+    response = requests.get(url)
+    resdata = response.text
+    return render(request, 'detail.html', {"resdata":resdata})
